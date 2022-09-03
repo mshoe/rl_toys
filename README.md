40ca2a29bc370bc1f1cd5ffff13ba3e4a4ff3c5b
mshoe <to.michaelxu@gmail.com>
2022-09-03 01:30:55 -0400

    grid world tabular q learning

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # rl_toys
+A collection of toy environments and algorithm implementations.
+
+# Algorithm implementations
+
+## Tabular Q learning on Grid World
+grid_world_tab_Q_learning.py<br/>
+![gif](gifs/tabular_q_learning_grid_world.gif)<br/>
---
+++ grid_world_tab_Q_learning.py
@@ -0,0 +1,102 @@
+'''
+Author: Michael Xu, 9/3/2022
+
+Tabular Q-Learning Algorithm:
+We want to learn the optimal Q function Q*(s, a)
+Reference: L2 Deep Q-Learning (Foundations of Deep RL Series)
+https://www.youtube.com/watch?v=Psrhxy88zww
+
+0. Initialize environment
+1. Initialize Q(s, a) for all states and actions
+2. sample initial state s
+3. Sample action a (epsilon-greedy)
+4. Take a step through the environment, and get next state s', reward, done signal
+5a. If done: estimate of Q*(s, a) = reward, reset initial state s
+5b. Else: estimate of Q*(s, a) = reward + discounted max(Q(s', ))
+6. Update Q(s, a) with estimate of Q*(s, a) scaled by a learning rate
+8. Repeat 3 - 7 for # of training steps
+'''
+
+import gym
+from envs.grid_world import GridWorldEnv
+import numpy as np
+import random
+
+# environment parameters
+grid_size = 5
+
+# training parameters
+eps = 0.1       # exploration likelihood
+gamma = 0.99    # discount factor
+alpha = 0.1     # learning rate
+
+
+# Set up Q(s, a)
+# state space S size is (n,n,n,n), which is (n,n) for agent position and (n,n) for target position
+# action space A size 4, so the Q space is (n,n,n,n,4)
+Q = np.zeros(shape=(grid_size, grid_size, grid_size, grid_size, 4))
+
+def simulate(steps=1000,train=False):
+    global Q
+    
+    # 0. initialize environment
+    mode = None if train else "human"
+    env = GridWorldEnv(size=grid_size,render_mode=mode)
+    env.action_space.seed(42)
+    
+
+    # 1. Initialize Q(s, a) for all states and actions
+    if train:
+        Q = np.zeros(shape=(grid_size, grid_size, grid_size, grid_size, 4))
+    
+    # 2. sample initial state s
+    observation, info = env.reset(seed=42)
+
+    # 8. Repeat 3 - 7 for # of training steps
+    for _ in range(steps):
+        agent_pos = observation["agent"]
+        target_pos = observation["target"]
+
+        # 3. Sample action a (epsilon-greedy)
+        action = 0
+        greedy = random.random() > eps
+        if greedy:
+            # argmax for best action used in greedy update
+            possible_returns = np.array(Q[tuple(np.concatenate((agent_pos, target_pos), axis = None))])
+
+            # Get all the actions that return the best value, then randomly select one
+            # If there is only one best value, then this is deterministic
+            best_action = np.random.choice(np.flatnonzero(possible_returns == possible_returns.max()))
+            action = best_action
+        else:
+            action = env.action_space.sample()
+
+        index = tuple(np.concatenate((agent_pos, target_pos, action), axis=None))
+        
+        # 4. Take a step through the environment, and get next state s', reward, done signal
+        observation, reward, done, info = env.step(action)
+        
+        estimate = 0
+        if done: # 5a. If done: estimate of Q*(s, a) = reward, reset initial state s
+            estimate = reward
+            observation, info = env.reset()
+        else: # 5b. Else: estimate of Q*(s, a) = reward + discounted max(Q(s', ))
+            new_agent_pos = observation["agent"]
+            new_target_pos = observation["target"]
+            possible_returns = np.array(Q[tuple(np.concatenate((new_agent_pos, new_target_pos), axis = None))])
+
+            estimate = reward + gamma * possible_returns.max()
+
+        # 6. Update Q(s, a) with estimate of Q*(s, a) scaled by a learning rate
+        if train:
+            Q[index] = (1.0 - alpha) * Q[index] + alpha * gamma * estimate
+
+        if env.render_mode == "human":
+            env.render()
+    env.close()
+
+print("training...")
+simulate(steps = 100000, train=True)
+print("testing...")
+eps = 0 # don't explore when testing
+simulate(steps = 1000, train=False)
